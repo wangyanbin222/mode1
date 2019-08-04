90411fda10cc6e2cc5c5fabcaca49ef44a62708b
wangyanbin222 <1572249042@qq.com>
2019-08-04 21:01:04 +0800

    前台登录

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+
+import login  from '../view/login/login'
+
+
+
+
+Vue.use(Router)
+
+export default new Router({
+  mode:'history',
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: login,
+        }
+      ]
+})
